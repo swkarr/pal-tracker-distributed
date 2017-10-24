9febee288303e3c9008cfdd3f6213a1e3b315185
Shawn Karr <swkarr@hotmail.com>
2017-10-24 08:02:28 -0700

    Configure resouce server for OAuth2

--- components/projects/src/main/java/io/pivotal/pal/tracker/projects/ProjectController.java
+++ components/projects/src/main/java/io/pivotal/pal/tracker/projects/ProjectController.java
@@ -15,7 +15,7 @@ import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/projects")
-public class ProjectController {
+public class    ProjectController {
 
     private final ProjectDataGateway gateway;
 
